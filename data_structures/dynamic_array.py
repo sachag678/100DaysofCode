ed052047d8769b25462ff0a53bdaee63f68b6061
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-07-19 23:58:39 -0400

    Added docstrings and updated the tests for the generic list type methods.

--- data_structures/dynamic_array.py
+++ data_structures/dynamic_array.py
@@ -1,13 +1,18 @@
+"""List class implementation."""
 import ctypes
 
+
 class DynamicArray(object):
-    
+    """List class."""
+
     def __init__(self, capacity=1):
+        """Initialize."""
         self.size = 0
         self.capacity = capacity
         self.array = self.make_array(self.capacity)
 
     def __repr__(self):
+        """String representation."""
         s = '['
         for i in range(self.size):
             s += str(self.array[i])
@@ -16,13 +21,22 @@ class DynamicArray(object):
         s += ']'
         return s
 
+    def __eq__(self, other):
+        """Check equality."""
+        for i in range(self.size):
+            if other[i] != self.array[i]:
+                return False
+        return True
+
     def extend(self, array):
+        """Concatenate one array to self."""
         for i in range(len(array)):
             self.append(array[i])
 
         return self.array
-    
+
     def insert(self, index, value):
+        """Insert a value at a specific index."""
         if index <= 0:
             new_array = self.make_array(self.capacity + 1)
             new_array[0] = value
@@ -39,45 +53,51 @@ class DynamicArray(object):
         return self.array
 
     def index(self, value):
+        """Return the index of a value from the array. Raise an exception if it is not in the array."""
         for i in range(self.size):
             if self.array[i] == value:
                 return i
-        return ValueError(str(value) + ' is not in the list')
+        raise ValueError(str(value) + ' is not in the list')
 
     def remove(self, value):
+        """Remove the first instance of a value."""
         value_index = self.index(value)
         removed_array = self.make_array(self.capacity)
-        
+
         for i in range(self.size - 1):
             if i >= value_index:
                 j = i + 1
             else:
                 j = i
             removed_array[i] = self.array[j]
-        
+
         self.array = removed_array
         self.size -= 1
+        return self.array
 
     def count(self, value):
+        """Count the number instances of value."""
         count = 0
         for i in range(self.size):
             if self.array[i] == value:
                 count += 1
         return count
-    
+
     def reverse(self):
+        """Reverse the array."""
         r_array = self.make_array(self.capacity)
         for i in range(self.size):
             r_array[i] = self.array[self.size - 1 - i]
 
         self.array = r_array
-        return
+        return self.array
 
     def make_array(self, capacity):
+        """Make an array with type python objects with length capacity."""
         return (capacity * ctypes.py_object)()
 
     def append(self, value):
-
+        """Add a value to the end of the array."""
         if self.capacity <= self.size:
             self._resize(2 * self.capacity)
 
@@ -85,20 +105,21 @@ class DynamicArray(object):
         self.size += 1
 
     def _resize(self, new_capactity):
-        
+
         self.temp_array = self.make_array(2 * self.capacity)
-        
+
         for i in range(self.size):
             self.temp_array[i] = self.array[i]
-        
+
         self.array = self.temp_array
         self.capacity = new_capactity
 
     def __len__(self):
+        """Return the number of elements in the array."""
         return self.size
-    
+
     def __getitem__(self, index):
-        
+        """Return an element from a specific index."""
         if index < 0 or index > self.size:
             raise IndexError("index is out of bounds")
 
@@ -115,4 +136,3 @@ if __name__ == '__main__':
     print(l)
     l.insert(-1, 1)
     print(l)
-    
--- data_structures/test_dynamic_array.py
+++ data_structures/test_dynamic_array.py
@@ -1,32 +1,36 @@
+"""Tests for dyamic array."""
+
 from dynamic_array import DynamicArray
 import unittest
 
+
 class TestDynamicArraymethods(unittest.TestCase):
-    
+    """Main Tests."""
+
     # test size of initialized array
     def test_array_empty(self):
         dynamic_array = DynamicArray()
         self.assertEqual(len(dynamic_array), 0)
-    
+
     # test append and __getitem__
     def test_array_has_specific_element(self):
         dynamic_array = DynamicArray()
         dynamic_array.append(2)
-        self.assertEqual(dynamic_array[0],2)
-    
+        self.assertEqual(dynamic_array[0], 2)
+
     # test append and len
     def test_array_has_one_element(self):
         dynamic_array = DynamicArray()
         dynamic_array.append(2)
-        self.assertEqual(len(dynamic_array),1)
-    
+        self.assertEqual(len(dynamic_array), 1)
+
     # test multiple appends
     def test_multiple_appends(self):
         dynamic_array = DynamicArray()
         dynamic_array.append(2)
         dynamic_array.append(3)
-        self.assertEqual(len(dynamic_array),2)
-    
+        self.assertEqual(len(dynamic_array), 2)
+
     # error checking
     def test_getting_index(self):
         dynamic_array = DynamicArray()
@@ -35,6 +39,51 @@ class TestDynamicArraymethods(unittest.TestCase):
             dynamic_array[3]
 
         self.assertEqual("index is out of bounds", cm.exception.args[0])
+
+    def test_count(self):
+        dynamic_array = DynamicArray()
+        dynamic_array.append(2)
+        dynamic_array.append(2)
+        dynamic_array.append(3)
+        self.assertEqual(dynamic_array.count(2), 2)
+
+    def test_insert(self):
+        pass
+
+    def test_remove(self):
+        dynamic_array = DynamicArray()
+        dynamic_array.append(1)
+        dynamic_array.append(2)
+        dynamic_array.append(3)
+
+        dynamic_array2 = DynamicArray()
+        dynamic_array2.append(1)
+        dynamic_array2.append(2)
+
+        self.assertEqual(dynamic_array.remove(3), dynamic_array2)
+
+    def test_reverse(self):
+        dynamic_array1 = DynamicArray()
+        dynamic_array1.append(2)
+        dynamic_array1.append(3)
+        dynamic_array2 = DynamicArray()
+        dynamic_array2.append(3)
+        dynamic_array2.append(2)
+
+        self.assertEqual(dynamic_array1.reverse(), dynamic_array2)
+
+    def test_extend(self):
+        dynamic_array1 = DynamicArray()
+        dynamic_array1.append(2)
+        dynamic_array2 = DynamicArray()
+        dynamic_array2.append(2)
+
+        dynamic_array3 = DynamicArray()
+        dynamic_array3.append(2)
+        dynamic_array3.append(2)
+
+        self.assertEqual(dynamic_array1.extend(dynamic_array2), dynamic_array3)
+
 
 if __name__ == '__main__':
     unittest.main()
