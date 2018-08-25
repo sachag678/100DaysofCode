423af05457712c645f0b5d501e390bdaf15dc6f3
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-08-25 18:23:21 -0400

    Added draw card method and docstrings.

--- data_structures/deck_of_cards.py
+++ data_structures/deck_of_cards.py
@@ -1,8 +1,15 @@
-"""Contains the Object Oriented implemention of a generic deck of cards."""
+"""Contains the Object Oriented implemention of a generic deck of cards.
+    name: Sacha Gunaratne
+    github: sachag678
+"""
+
+import random
 
 class Card():
+    """Generic card class."""
 
     def __init__(self, suit, rank):
+        """Initialize each card with a suit and rank."""
         self.suit = suit
         self.rank = rank
     
@@ -22,8 +29,10 @@ class Card():
         return id(self)
 
 class Deck():
+    """Generic deck implementation."""
 
     def __init__(self):
+        """Create a standard 52-card deck of cards."""
         self.cards = set()
         suits = ['Heart', 'Diamond', 'Spade', 'Club']
         ranks = [i for i in range(2, 15)]
@@ -37,6 +46,17 @@ class Deck():
             s += str(card)
             s += '\n'
         return s
+    
+    def draw(self, number_of_cards=1):
+        """Draw a card randomly from the deck. Simulates a shuffled deck without having an ordered deck."""
+        drawn_cards = []
+        for _ in range(number_of_cards):
+            drawn_card = random.choice(self.cards)
+            self.cards.remove(drawn_card)
+            drawn_cards.append(drawn_card)
+
+        return drawn_cards
+    
 
 if __name__ == '__main__':
     deck = Deck()
