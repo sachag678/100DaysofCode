5f9eba49c6d389d207924d50d3ce50a530bb2605
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-12-17 17:17:31 -0500

    Small changes to the code.

--- learn_c++/factorial.cpp
+++ learn_c++/factorial.cpp
@@ -6,15 +6,14 @@ using namespace std::chrono;
 int main(){
     auto start = high_resolution_clock::now();
 
-    int n = 200000;
-    long double sum = 1;
+    unsigned int n = 200000;
+    unsigned long long sum = 1;
     for(int i=2; i <= n; ++i){
-    	std::cout <<"output : " << sum << std::endl;
         sum = sum * i;
     }
 
     auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<seconds>(stop - start);
+    auto duration = duration_cast<microseconds>(stop - start);
 	
     std::cout <<"output : " << sum << std::endl;
     std::cout <<"iterative_factorial : " << duration.count() << std::endl;
