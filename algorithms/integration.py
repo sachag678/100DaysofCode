72bb84c7818e091c6e67d437a022110edd61771c
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2019-03-24 22:10:03 -0400

    Implemented trapazoidal integration numerically.

--- algorithms/integration.py
+++ algorithms/integration.py
@@ -1,27 +1,28 @@
-
 import numpy as np
 
-def trap(a,b,k):
-	'''This function approximates the integration numerically
-	using the trapezod method
-	params: a - limit0
-			b - limit1
-			k - number of intervals or rectangles to place under the curve
-	return: the numerical integration over the limit
 
-	change the function y to whatever the user wants
-	'''
-	sum =0
-	for i in range(k+1):
-		x = a+(b-a)*i/k
-		y = np.cos(x)
-		if i==0 or i==k:
-			y = y/2
-		sum+=y
+def trap(a, b, k):
+    '''This function approximates the integration numerically
+    using the trapezod method
+    params: a - limit0
+            b - limit1
+            k - number of intervals or rectangles to place under the curve
+    return: the numerical integration over the limit
 
-	sum = sum*(b-a)/k
-	return sum
+    change the function y to whatever the user wants
+    '''
+    sum = 0
+    for i in range(k+1):
+        x = a+(b-a)*i/k
+        y = np.cos(x)
+        if i == 0 or i == k:
+            y = y/2
+        sum += y
 
-print(trap(-1,1,1000000))
+    sum = sum*(b-a)/k
+    return sum
+
+
+print(trap(-1, 1, 1000000))
 
 print(np.sin(1)-np.sin(-1))
